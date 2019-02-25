3d66aba2fa52e49310a91fcc3c48c0fbf2617d12
Waleed Elsakka <waleedpahome@live.com>
2019-02-25 12:43:46 -0500

    Takes user input wallet address as arg

--- getWallet.py
+++ getWallet.py
@@ -4,10 +4,9 @@ import json
 #Use of Etherscan API to pull wallet balances
 #Initialize function with your API key and address
 
-def walletBalance():
+def walletBalance(walletAddress):
     api_key = 'IVPR3T2CWJ2TKUYH6824HNGKKJ4ENEM9EU'
-    address = '0x29126c4099c2d6e1dEBE2529CC5D983E5ed6fD7C'
 
-    api = Account(address=address, api_key=api_key)
+    api = Account(address=walletAddress, api_key=api_key)
     balance = api.get_balance()
     return balance
